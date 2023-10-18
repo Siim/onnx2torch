df9c86f5af6c04413095d9795b000986e318b97f
Siim Haugas <haugas@gmail.com>
2023-10-18 15:53:53 +0300

    Update clip.py

--- onnx2torch/node_converters/clip.py
+++ onnx2torch/node_converters/clip.py
@@ -44,33 +44,6 @@ def _create_torch_module(min_val: Optional[torch.Tensor], max_val: Optional[torc
 
     return torch_module
 
-
-@add_converter(operation_type='Clip', version=11)
-@add_converter(operation_type='Clip', version=12)
-@add_converter(operation_type='Clip', version=13)
-def _(node: OnnxNode, graph: OnnxGraph) -> OperationConverterResult:
-    # Min and Max inputs are optional
-    min_name = node.input_values[1] if len(node.input_values) > 1 else None
-    max_name = node.input_values[2] if len(node.input_values) > 2 else None
-
-    try:
-        min_val = float(get_const_value(min_name, graph)) if min_name is not None else None
-        max_val = float(get_const_value(max_name, graph)) if max_name is not None else None
-    except KeyError as exc:
-        raise NotImplementedError('Dynamic value of min/max is not implemented') from exc
-
-    torch_module = _create_torch_module(min_val=min_val, max_val=max_val)
-
-    return OperationConverterResult(
-        torch_module=torch_module,
-        onnx_mapping=OnnxMapping(
-            inputs=(node.input_values[0],),
-            outputs=node.output_values,
-        ),
-    )
-
-
-
 class DynamicClip(torch.nn.Module):
     def __init__(self):
         super(DynamicClip, self).__init__()
